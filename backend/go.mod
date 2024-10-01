96378f41c89f708f33776ebb95ab461c3845919d
Seymur Bagirov <seymur.baghirov@proton.me>
2024-10-01 23:36:57 +0400

    feat: add configuration support through koanf

--- backend/go.mod
+++ backend/go.mod
@@ -3,11 +3,21 @@ module elytra.com/backend
 go 1.23.1
 
 require (
+	github.com/knadh/koanf/parsers/yaml v0.1.0
+	github.com/knadh/koanf/providers/file v1.1.0
+	github.com/knadh/koanf/v2 v2.1.1
 	github.com/labstack/echo/v4 v4.12.0
 	github.com/rs/zerolog v1.33.0
 )
 
-require github.com/santhosh-tekuri/jsonschema/v5 v5.3.1 // indirect
+require (
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/knadh/koanf/maps v0.1.1 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/santhosh-tekuri/jsonschema/v5 v5.3.1 // indirect
+)
 
 require (
 	github.com/bahlo/generic-list-go v0.2.0 // indirect
@@ -26,7 +36,7 @@ require (
 	github.com/wk8/go-ordered-map/v2 v2.1.9-0.20240815153524-6ea36470d1bd // indirect
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"net/http"
+	"errors"
+	"fmt"
 	"os"
 
 	"github.com/labstack/echo/v4"
@@ -9,16 +10,19 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/v2"
+
 	"elytra.com/backend/docs"
 	"elytra.com/backend/features/user/getUser"
 )
 
-func setupRoutes(e *echo.Echo) {
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	docs.RegisterDocs(e, "./docs/spec.yaml")
-	e.File("/elytra.png", "./elytra.png")
+func setupRoutes(e *echo.Echo, conf Config) {
+	if conf.Mode == "dev" {
+		docs.RegisterDocs(e, "./docs/spec.yaml")
+		e.File("/elytra.png", "./elytra.png")
+	}
 	getUser.RegisterGetUser(e)
 }
 
@@ -41,19 +45,47 @@ func setupLogger(e *echo.Echo) {
 	}))
 }
 
-func bootstrap(e *echo.Echo) {
+func bootstrap(e *echo.Echo, conf Config) {
 	setupLogger(e)
-	setupRoutes(e)
+	setupRoutes(e, conf)
+}
+
+type Config struct {
+	AppPort string `koanf:"appPort"`
+	Mode    string `koanf:"appMode"`
+}
+
+func getConfig() (Config, error) {
+	koanf := koanf.New(".")
+	var conf Config
+
+	if err := koanf.Load(file.Provider("config.yaml"), yaml.Parser()); err != nil {
+		return conf, errors.New(fmt.Sprintf("Couldn't load the config: %v", err))
+	}
+
+	if err := koanf.Unmarshal("", &conf); err != nil {
+		return conf, errors.New(fmt.Sprintf("Couldn't marshal the config: %v", err))
+	}
+
+	return conf, nil
 }
 
 func main() {
 	e := echo.New()
 
-	bootstrap(e)
+	conf, err := getConfig()
 
-	e.Use(redirectToDocs)
+	if err != nil {
+		log.Fatal().Msg(err.Error())
+	}
 
-	if err := e.Start(":9000"); err != nil {
+	bootstrap(e, conf)
+
+	if conf.Mode == "dev" {
+		e.Use(redirectToDocs)
+	}
+
+	if err := e.Start(fmt.Sprintf(":%v", conf.AppPort)); err != nil {
 		log.Fatal().Msg(err.Error())
 	}
 }
