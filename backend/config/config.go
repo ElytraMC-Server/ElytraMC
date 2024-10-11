ddaf45c352aee61dadf55479485c82e52481dede
Seymur Bagirov <seymur.baghirov@proton.me>
2024-10-11 05:09:25 +0400

    refactor: divide config files now there is secrets.yaml(which is hidden from source control) and launchConfig.yaml, which shouldn't be hidden at all

--- backend/config/config.go
+++ backend/config/config.go
@@ -22,16 +22,34 @@ type Config struct {
 	DbConn  string  `koanf:"dbConn"`
 }
 
+type ConfigLoadError struct {
+	errors []error
+}
+
+func (loadErr *ConfigLoadError) Error() string {
+	return fmt.Sprintf("Got error(s) while loading configuration: %v", loadErr.errors)
+}
+
 func GetConfig() (Config, error) {
 	koanf := koanf.New(".")
 	var conf Config
 
-	if err := koanf.Load(file.Provider("config/config.yaml"), yaml.Parser()); err != nil {
-		return conf, errors.New(fmt.Sprintf("Couldn't load the config: %v", err))
+	var loadErrs []error
+
+	if err := koanf.Load(file.Provider("config/secrets.yaml"), yaml.Parser()); err != nil {
+		loadErrs = append(loadErrs, errors.New(fmt.Sprintf("Couldn't load the secrets: %v", err)))
+	}
+
+	if err := koanf.Load(file.Provider("config/launchConfig.yaml"), yaml.Parser()); err != nil {
+		loadErrs = append(loadErrs, errors.New(fmt.Sprintf("Couldn't load the launch configuration: %v", err)))
+	}
+
+	if len(loadErrs) > 0 {
+		return Config{}, &ConfigLoadError{errors: loadErrs}
 	}
 
 	if err := koanf.Unmarshal("", &conf); err != nil {
-		return conf, errors.New(fmt.Sprintf("Couldn't marshal the config: %v", err))
+		return Config{}, errors.New(fmt.Sprintf("Couldn't marshal the config: %v", err))
 	}
 
 	return conf, nil
